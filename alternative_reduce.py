8a343872e5d89e460696bb46c8192c530933f323
pingff <lfong24@students.claremontmckenna.edu>
2023-05-01 01:34:40 -0700

    alt 2

--- alternative_reduce.py
+++ alternative_reduce.py
@@ -14,13 +14,12 @@ parser.add_argument('--input_dir', required=True)
 parser.add_argument('--keys', nargs='+', required=True)
 args = parser.parse_args()
 
-input_files = glob(args.input_dir + '/*')
 
 for key in args.keys:
     yaxis = []
     total = defaultdict(lambda: Counter())
 
-    for path in sorted(input_files):
+    for path in sorted(glob(args.input_dir + '/*')):
         with open(path) as f:
             tmp = json.load(f)
             sumnum = 0
@@ -30,10 +29,13 @@ for key in args.keys:
             except:
                 pass
             yaxis.append(sumnum)
+
     plt.plot(np.arange(len(yaxis)), yaxis, label=key)
 
 plt.xlabel("Date in 2020")
 plt.ylabel("Times Mentioned in Tweets")
+plt.title("Times a Hastag was mentioned in Tweets by day in 2020")
 plt.legend()
+plt.xticks([0, 60, 121, 182, 244, 305], ["Jan", "Mar", "May", "Jul", "Sept", "Nov"])
 plt.savefig("sickvshospital_tweet.png")
 plt.show()
