1426849e932375e1896adb0f00c38ed35e9aa32f
pingff <lfong24@students.claremontmckenna.edu>
2023-05-01 14:28:37 -0700

    11

--- alternative_reduce.py
+++ alternative_reduce.py
@@ -23,36 +23,32 @@ results = {}
 for path in paths:
         date_str = os.path.splitext(os.path.basename(path))[0][10:18]
         date = datetime.strptime(date_str, '%y-%m-%d')
-        try:
-            with open(path) as f:
+        with open(path) as f:
+            try:
                 data = json.load(f)
                 for key in args.key.split(','):
                     if key in data:
                         if date not in results:
                             results[date] = defaultdict(int)
                         results[date][key] += sum(data[key].values())
-        except json.decoder.JSONDecodeError:
-            print(f"Skipping invalid JSON file: {path}")
-
+            except json.decoder.JSONDecodeError:
+                print(f"Skipping invalid JSON file: {path}")
 results = dict(sorted(results.items()))
 print("results=", results)
-
 keys = set()
+
 for date, data in results.items():
     keys.update(data.keys())
 keys = list(keys)
-print("keys=", keys)
 for key in keys:
-    print("key =", key )
-    x = list(results.keys())
-    y = [results[date][key] for date in x]
-    x = [date.date() for date in x]  # convert to date objec
+    x = [date.date() for date in results.keys()]
+    y = [results[date][key] for date in results.keys()]
     plt.plot(x,y,label = key)
+
 
 plt.xlabel("Date in 2020")
 plt.ylabel("Times Mentioned in Tweets")
 plt.title("Times a Hastag was mentioned in Tweets by day in 2020")
-plt.xticks(x[::60], [d.strftime('%b %d') for d in x[::60]])
 plt.legend()
 plt.savefig("sickvhospital_tweet.png")
 plt.show()
