514e2fb2d4ca1e286a34931402c28f1f0587ed19
pingff <lfong24@students.claremontmckenna.edu>
2023-05-01 01:30:50 -0700

    add alt

--- alternative_reduce.py
+++ alternative_reduce.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python3
-import argparse
+import matplotlib
+import numpy as np
 import json
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import argparse
 from collections import Counter, defaultdict
 from glob import glob
 
-import matplotlib
-import numpy as np
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+# command line args
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_dir', required=True)
+parser.add_argument('--keys', nargs='+', required=True)
+args = parser.parse_args()
 
+input_files = glob(args.input_dir + '/*')
 
-def main(args):
-    input_files = glob(args.input_dir + '/*')
-    for key in args.keys:
-        yaxis = []
-        total = defaultdict(lambda: Counter())
+for key in args.keys:
+    yaxis = []
+    total = defaultdict(lambda: Counter())
 
-        for path in sorted(input_files):
-            with open(path) as f:
-                tmp = json.load(f)
-                num = 0
-                try:
-                    for k in tmp[key]:
-                        num += tmp[key][k]
-                except:
-                    pass
-                yaxis.append(num)
-        plt.plot(np.arrange(len(yaxis)), yaxis, label=key)
+    for path in sorted(input_files):
+        with open(path) as f:
+            tmp = json.load(f)
+            sumnum = 0
+            try:
+                for k in tmp[key]:
+                    sumnum += tmp[key][k]
+            except:
+                pass
+            yaxis.append(sumnum)
+    plt.plot(np.arange(len(yaxis)), yaxis, label=key)
 
 plt.xlabel("Date in 2020")
 plt.ylabel("Times Mentioned in Tweets")
 plt.legend()
-plt.xticks([0, 60, 121, 182, 244, 305], ["Jan", "Mar", "May", "Jul", "Sept", "Nov"])
 plt.savefig("sickvshospital_tweet.png")
+plt.show()
